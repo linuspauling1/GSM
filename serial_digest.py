1646bc65e19ec092578a84ace1f2d772cb6dc2e9
linuspauling1 <paulb2233@yahoo.com>
2024-04-27 19:57:12 +0300

    Introduced Gotify for asynchronous serial digest acknowledgement

--- serial_digest.py
+++ serial_digest.py
@@ -1,30 +1,53 @@
 import serial
+from gotify import Gotify
+
+gotify = Gotify(
+    base_url='http://localhost:80',
+    app_token='A_8APqsvE8C5Rom',
+)
+
+last_dialing_number = 0
 
 with serial.Serial('/dev/ttyS0', baudrate=115200) as ser:
     while True:
+        global call_status
         data = ser.read_until().decode()
+        
         if data == 'RING\r\n':
-            title='Ring'
+            call_status='Ring'
             ser.write(b'AT+CLCC\r\n')
-            print(title)
+            print(call_status)
             print()
-        if data == 'NO CARRIER\r\n':
-            title='Hang'
-            print(title)
+        elif data == 'NO CARRIER\r\n':
+            call_status='Hang'
+            print(call_status)
             print()
+            gotify.create_message(
+                message=call_status,
+                title=last_dialing_number,
+            )
+
         if data.startswith('+CMT'):
             [number,_,date,hour] = [x.strip('"') for x in data[len('+CMT: '):-len('\r\n')].split(',')]
-            title='Message'
             message = ser.read_until(b'\r\n').decode()[:-2]
+            message += '\n' + date + ' ' + hour
+            title = number[2:]
             print(title)
-            print(number[2:])
-            print(date + ' ' + hour)
             print(message)
             print()
+            gotify.create_message(
+                message=message,
+                title=title,
+            )
+
         if data.startswith('+CLCC'):
             for x in data[len('+CLCC: '):-len('\r\n')].split(','):
                 if x.startswith('"') and x.endswith('"'):
-                    number = x.strip('"')
+                    last_dialing_number = number = x.strip('"')
                     print(number)
                     print()
+                    gotify.create_message(
+                        message=call_status,
+                        title=number,
+                    )
                     break
